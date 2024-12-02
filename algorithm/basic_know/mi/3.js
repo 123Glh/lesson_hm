b8abd58ee115c11663655624a0cb86c78705b237
123Glh <731031976@qq.com>
2024-12-02 21:33:14 +0800

    提交了关于递归和快速幂的讲解

---
+++ algorithm/basic_know/mi/3.js
@@ -0,0 +1,14 @@
+//求x的n次方
+// 快速幂算法:
+function func(x, n) {
+  if (n === 0) {
+    return 1
+  } else if (n % 2 === 0) {
+    let result = func(x, n / 2)
+    return result * result
+  } else {
+    let result = func(x, (n - 1) / 2)
+    return result * result * x
+  }
+}
+console.log(func(2, 11))
