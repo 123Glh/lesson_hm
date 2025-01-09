6538e20cd442ccb49f08541613d12cee42cd8877
123Glh <731031976@qq.com>
2025-01-09 19:02:34 +0800

    jsonp解决跨域

---
+++ js/jsonp/server.js
@@ -0,0 +1,32 @@
+// http 服务启动
+// 内置的 http 模块
+// commonjs node早期  es6模块化
+const http = require('http')
+// 启动 http 服务
+// 基于请求/响应的简单协议
+const users = [
+  {
+    id: 1,
+    name: 'zs'
+  },
+  {
+    id: 2,
+    name: 'ls'
+  },
+  {
+    id: 3,
+    name: 'ww'
+  },
+]
+const server = http.createServer((req, res) => {
+  // 异步 高并发
+  // 请求来到服务器后，该函数会被执行
+  // req 请求对象解析
+  // 拿到资源
+  // res 响应对象
+  // http 结束
+  res.end('callback(' + JSON.stringify(users) + ')')
+})
+server.listen(3000, () => {
+  console.log('server is running at port 3000')
+})
